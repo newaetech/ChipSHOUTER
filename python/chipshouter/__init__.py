49f22d5ac922d75e084ce5233e57ab2bb0816878
Colin O'Flynn <coflynn@newae.com>
2018-07-14 18:41:46 -0300

    Move python to new repo and submodule

--- python/chipshouter/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["chipshouter"]
-
-from chipshouter import ChipSHOUTER
--- python/scripts/shouter-console.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from chipshouter.console.console import main
-
-if __name__ == "__main__":
-    main()
