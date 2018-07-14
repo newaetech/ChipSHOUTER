e2c1b5f91d3d6b85b414a53566c178273a450771
Colin O'Flynn <coflynn@newae.com>
2018-07-14 17:51:41 -0300

    Fix sphinx config file

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -19,7 +19,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../chipshouter'), os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ChipSHOUTER'
-copyright = u'2018, NewAE Technology Inc.'
+copyright = u'2017-2018, NewAE Technology Inc.'
 author = u'NewAE Technology Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
